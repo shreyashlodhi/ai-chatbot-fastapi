cf5efa8b7bdce6f86800e94020d0a6b54f89ccbf
Shreyash Lodhi <shreyashlodhi2305@gmail.com>
2025-10-05 18:14:15 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -18,8 +18,8 @@ elif provider == "OpenAI":
 allow_web_serach = st.checkbox("Allow Web Search")
 user_query = st.text_area("Enter your query: ", height=150, placeholder="Ask Anything!")
 
-# API_URL="http://127.0.0.1:9999/chat"
-API_URL = "http://0.0.0.0:8080/chat"
+API_URL="http://127.0.0.1:9998/chat"
+# API_URL = "http://0.0.0.0:8080/chat"
 
 if st.button("Ask Agent!"):
     if user_query.strip():
